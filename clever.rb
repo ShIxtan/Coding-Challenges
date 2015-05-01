e4a899f8d4220a685247b8de5123365fce0e0f6f
ShIxtan <aulbrich@iu.edu>
2015-04-30 19:16:10 -0700

    did clever challenge

---
+++ clever.rb
@@ -0,0 +1,22 @@
+require 'net/https'
+require 'json'
+uri = URI('https://api.clever.com/v1.1/districts/4fd43cc56d11340000000005/sections')
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+
+request = Net::HTTP::Get.new(uri.request_uri)
+request.add_field 'Authorization', 'Bearer DEMO_TOKEN'
+
+response = http.request(request)
+
+x = JSON.parse(response.body)
+
+sum = 0.0
+
+x["data"].each do |section|
+  sum += section["data"]["students"].count
+end
+
+puts sum / x["data"].count
